d154ba79ecaecb21a3f345dec855c56c9c96796b
hikurangi <hikurangi@gmail.com>
2017-08-13 15:17:24 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// const util = require('util')
+// require('child_process').spawn('clip').stdin.end(util.inspect("content_for_the_clipboard")); // for Windows
+// check os for clipboard?
+
+const unicodeConverter = (direction, str) => {
+  console.log({direction, str});
+  if (direction.toLowerCase() === 's') {
+    // copy to clipboard
+    console.log('Your string is:', String.fromCharCode(str))
+  } else if (direction.toLowerCase() === 'c') {
+    // copy to clipboard
+    console.log('Your unicode values are', str.split('').map(char => char.charCodeAt(0)).join(' '))
+  } else {
+    console.log("Invalid parameter - please choose 's' for unicode -> string conversion and c for string -> unicode conversion.")
+  }
+}
+
+unicodeConverter(process.argv[2], process.argv[3])
+
+module.exports = unicodeConverter // available externally

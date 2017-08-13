023789aca3f2fd590aaf6550a659626db71f9e94
hikurangi <hikurangi@gmail.com>
2017-08-13 15:58:31 +1200

    it works

--- index.js
+++ index.js
@@ -4,13 +4,12 @@
 
 const unicodeConverter = (direction, arr) => {
   console.log({direction, arr});
-  str = arr.join(' ').split('') // makes the sliced remainder of the process.argv array into a string where array entries are space-separated, then splits this string into individual array entries.
   if (direction.toLowerCase() === 's') {
     // copy to clipboard
-    console.log('Your string is:', str.map(code => String.fromCharCode(code)).join(''))
+    console.log('Your string is:', arr.map(code => String.fromCharCode(code)).join(''))
   } else if (direction.toLowerCase() === 'c') {
     // copy to clipboard
-    console.log('Your unicode values are', str.map(char => char.charCodeAt(0)).join(' '))
+    console.log('Your unicode values are', arr.join(' ').split('').map(char => char.charCodeAt(0)).join(' '))
   } else {
     console.log("Invalid parameter - please choose 's' for unicode -> string conversion and c for string -> unicode conversion.")
   }
